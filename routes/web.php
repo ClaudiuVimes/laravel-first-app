62c070464910cc380c70f9baf8c75258c39b5022
Claudiu Vimes <claudiucerna@gmail.com>
2022-12-02 11:10:57 +0200

    first route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    Debugbar::warning('info');
-    return view('welcome');
-});
 
+Route::get('/blog', [PostsController::class, 'index']);
