1416699952cc4c6469672ab23601c04d02de030c
Claudiu Vimes <claudiucerna@gmail.com>
2023-01-07 19:03:00 +0200

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,64 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\PostsController;
+
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Barryvdh\Debugbar\Twig\Extension\Debug;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+   Controller,
+   PostsController,
+   HomeController
+};
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+   GET - Request a resource
+   POST - Create a new resource
+   PUT - Update a resource - Updates the entire row
+   PATCH - Modify a resource - Only the changed resource
+   DELETE - Deletes a resource
+   OPTION - Asks the server which verbs are allowed
 */
 
 
+// GET 
+
+
+// POST 
+
+
+// PUT OR PATCH 
+
+
+// DELETE
+
+
+Route::prefix('/blog')->group(function () {
+   Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+   Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+
+   Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+   Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+
+   Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+   Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+   
+   Route::delete('/edit/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
+});
+
+//Multiple HTTP Verbs
+// Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+
+// Route::resource('blog', PostsController::class);
+
+// Route for invoke method
+Route::get('/', HomeController::class);
+
+
+// Route::any('/blog', [PostsController::class, 'index']);
+
+// Return view
+
+// Route::view('/blog', 'blog.index', ['name' => 'Code with Dary']);
+
+
 // Route::get('/blog', [PostsController::class, 'index']);
-Route::resource('blog', PostsController::class);
